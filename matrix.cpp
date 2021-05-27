796f8e472f380aaad461c58c5bd7b00634ba5199
FedericaCappucci <48451133+FedericaCappucci@users.noreply.github.com>
2021-05-27 21:08:25 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-const int ROW=900;
-const int COL=900;
+const int ROW=500;
+const int COL=500;
 //1 not obstacle 0 is obstacle
 int matrix[ROW][COL];
 bool exi(int i,int c)
