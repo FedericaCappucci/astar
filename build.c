e30243c568cd1f948e83f7eebe82fe75dd317e11
FedericaCappucci <48451133+FedericaCappucci@users.noreply.github.com>
2021-07-02 16:31:31 -0400

    build matrix

---
+++ build.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void buildMatrix(int row, int col) {
+	FILE *file;
+	file = fopen("Matrix.txt", "w");
+	if(file == NULL) {
+		printf("Errore apertura file\n");
+		exit(1);
+	} else {
+		srand(time(0));
+		for (int i = 0; i < row; i++) {
+	        for (int j = 0; j < col; j++) {
+	            if ((rand() & 1) | (rand() & 1)) {
+	            	fprintf(file, "%d", 1);
+	            } else {
+	            	fprintf(file, "%d", 0);
+	            }
+	        }
+	        fprintf(file, "\n");
+	    }
+	}
+}
+
+int main()
+{
+	buildMatrix(4000,4000)
+	return 0;
+}
