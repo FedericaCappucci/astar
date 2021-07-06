5e901199ddc8f2e49b5bd27c1f976c2b1a829e53
SaraMacri <44321685+SaraMacri@users.noreply.github.com>
2021-07-06 17:55:30 +0200

    Codice documentato

--- matrix.cpp
+++ matrix.cpp
@@ -6,34 +6,14 @@
 using namespace std;
 using std::endl;
 
-/*const int ROW=4000;
-const int COL=4000;
-int destR;
-int destC;*/
-
-
-
-//1 not obstacle 0 is obstacle
-//int matrix[ROW][COL];
+//This will contain the generated grid 
 int *Matrix;
-
-/*bool exi(int i,int c,int ROW)
-{
-	if((i!=0)&&(c!=0))
-	{
-		if((Matrix[(i-1)*ROW+c]==0)&&(Matrix[i*ROW+(c-1)]==0)&&(Matrix[(i+1)*ROW+(c-1)]==0)&&(Matrix[(i-1)*ROW+(c-1)]==0))
-		//if((matrix[(i-1)*ROW+c]==0)||(matrix[i*ROW+(c-1)]==0)||(matrix[(i+1)*ROW+(c-1)]==0)||(matrix[(i-1)*ROW+(c-1)]==0))
-		{
-			return false;
-		}
-	}
-	return true;
-	
-}*/
-int random_int(int min, int max)
-{
-   return min + rand() % (max+1 - min);
-}
+/*
+	generateMatrix generates a grid by filling it with 1(not obstacle)with a probability of 75% and 0(obstacle) with probability of 15%
+	INPUTS: 
+		-int row: number of rows of the generated grid
+		-int col: number of columns of the generated grid
+*/
 void generateMatrix(int row,int col)
 {
 	Matrix = (int*) calloc(row*col,sizeof(int));
@@ -42,41 +22,32 @@ void generateMatrix(int row,int col)
 		{
 		       for(int c=0;c<col;c++)
 		       {   		
-
-			       		int v2 = rand() % 100 + 1;
-			       		//cout<< "generate : " << v2 <<"\n";
-			       		//bool valid = exi(i,c,row);
-			       		if((v2>=0)&&(v2<75))
-			       		{
-			       			//then not obstacle
-			       			//if(valid==true)
-			       			//	cout<<"uno: ";
-			       				//my_file << "1" ;
-			       				Matrix[i*row+c]=1;
-			       				//cout<<Matrix[i*row+c]<<"\n";
-			       		}
-			       		else
-			       		{
-			       			//obstacle
-			       			//if(valid==true)
-			       		//		cout<<"zero: ";
-			       				Matrix[i*row+c]=0;
-			       			//	cout<<Matrix[i*row+c]<<"\n";
-			       				//my_file << "0";
-			       		}
-			       		//exi(i,c);
-			       
-		       }
-		       //my_file << endl;
+				int v2 = rand() % 100 + 1;
+				if((v2>=0)&&(v2<75))
+				{
+			       		//then not obstacle
+			       		Matrix[i*row+c]=1;
+			       	}
+			       	else
+			       	{
+			       		//obstacle
+			       		Matrix[i*row+c]=0;
+			       	}      
+		       }		       
 		}
-		//my_file.close();
-	
 }
+
+/*
+	writeFile writes in "Matrix.txt" the grid that was generated before
+	INPUTS:
+		-int ROW: number of rows of the generated grid we write in the file
+		-int COL: number of columns of the generated grid in the file
+*/
 void writeFile(int ROW,int COL)
 {
 	ofstream my_file("Matrix.txt",std::ofstream::out);
 	//exception handling
-  try {
+  	try {
 		if (my_file.is_open())
 	  	{
 	  		for(int i=0;i<ROW;i++)
@@ -103,40 +74,6 @@ void writeFile(int ROW,int COL)
 	
 }
 
-/*void printmatrix()
-{
-	for(int i=0;i<ROW;i++)
-	{
-	       for(int c=0;c<COL1;c++)
-	       {
-	       		cout <<" "<<Matrix[i*ROW1+c];
-	       	
-	       }
-	    cout<<"\n";
-	}
-}*/
-bool valida(int riga, int colonna,int ROW)
-{
-	if(Matrix[riga*ROW+colonna]==1)
-	{	
-		return true;
-	}
-	return false;
-}
-/*void generateDest()
-{
-	srand(time(NULL));
-	int riga;
-	int colon;
-	do{
-		riga = random_int(ROW1-3,ROW1);
-		colon = random_int(COL1-3,COL1);
-		
-	}
-	while(valida(riga,colon)!=true);
-	destR=riga; 
-	destC=colon;
-}*/
 /*int main()
 {
 	generateMatrix(4000,4000);
